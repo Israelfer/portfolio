5673e4a375ca70a74e83f92cd8bd22b5b7156789
unknown <israel.fbb@gmail.com>
2022-05-21 11:03:13 -0300

    Home Hero criada

---
+++ src/components/HomeHero/index.tsx
@@ -0,0 +1,40 @@
+import { CodeItem, Container, InfosContainer, TextContainer } from './styles';
+import picture from '../../assets/profile-Israel.jpg';
+
+function HomeHero() {
+  return (
+    <Container>
+      <img src={picture} alt="Foto de perfil de Israel" />
+      <div>
+        <TextContainer>
+          <h1>Hey,</h1>
+          <h2>me chamo Israel</h2>
+        </TextContainer>
+        <InfosContainer>
+          <CodeItem>
+            <span className="comment">//Minha apresentação</span>
+            <span className="purple">Infos </span>
+            {'\u007B'}
+            <div>
+              Nome: <span className="blue">Israel Batista</span>
+            </div>
+            <div>
+              Idade: <span className="blue">24 anos</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+          <CodeItem>
+            <span className="purple">Cargo </span>
+            {'\u007B'}
+            <div>
+              Função: <span className="blue">Desenvolvedor Front-end</span>
+            </div>
+            {'\u007D'}
+          </CodeItem>
+        </InfosContainer>
+      </div>
+    </Container>
+  );
+}
+
+export default HomeHero;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
 import Header from '../components/Header';
+import HomeHero from '../components/HomeHero';
+import { HomeContainer } from '../styles/HomeStyles';
 
 export default function Home() {
-  return <Header />;
+  return (
+    <HomeContainer>
+      <Header />
+
+      <main className="container">
+        <HomeHero />
+      </main>
+    </HomeContainer>
+  );
 }
